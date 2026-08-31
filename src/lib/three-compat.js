cb26f99ffefcad998b022ac6abc5ed5a3ae101a2
gojii <gojiinu@hotmail.com>
2026-08-30 19:17:54 -0700

    fix(build): fix Three.js Timer export path for Linux/Vercel webpack bundling

--- src/lib/three-compat.js
+++ src/lib/three-compat.js
@@ -1,4 +1,4 @@
-export * from "../../node_modules/three/build/three.module.js";
-export { Timer, FixedTimer } from "../../node_modules/three/examples/jsm/misc/Timer.js";
-import * as THREE from "../../node_modules/three/build/three.module.js";
+export * from "three/build/three.module.js";
+export { Timer, FixedTimer } from "three/examples/jsm/misc/Timer.js";
+import * as THREE from "three/build/three.module.js";
 export default THREE;
